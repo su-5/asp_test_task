97355f12ec1096f1e46da254b59ac619543b50bc
Andrei-PC <Andrei-PC@DESKTOP-NMVCPT8>
2017-08-12 01:42:02 +0300

    Project division into levels

--- Music_Directory/Global.asax.cs
+++ Music_Directory/Global.asax.cs
@@ -15,7 +15,6 @@ namespace Music_Directory
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
